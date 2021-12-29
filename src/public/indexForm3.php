17b2e24eacdbe145155e328393d1a8fd8c57ac9c
Art <sergejm422@gmail.com>
2021-12-29 11:57:23 +0200

    First commit

--- src/public/indexForm3.php
+++ src/public/indexForm3.php
@@ -1,18 +1,29 @@
 <?php
+/** @var TYPE_NAME $answer */
+$answer = "Голубой";
+
+$p=0;
+
+if($answer == $_POST['blue']){
+    echo "Верно", "<br/>";
+    $p = $p + 1;
+
+}
+
 $k=0;
 if (is_array($_POST['answer'])){
     foreach($_POST['answer'] as $value)
     {
-        $answer = "Голубой";
+
         if ($value=='Верно') {
             $k = $k + 1;
-        }elseif($answer == $_POST['blue']){
-            $k = $k +1;
-            echo 'Верно';
-            print ($_POST['blue']);
         }
+
 
         echo $value, '<br>';
     }
-    echo "У вас {$k} правильных ответов", '<br>';
 }
+
+$z = $k + $p;
+
+echo "У вас {$z} правильных ответов", '<br>';
