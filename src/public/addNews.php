18ad2a2cbd41979295d29e995a942da01760434a
Art <sergejm422@gmail.com>
2022-01-21 18:00:14 +0200

    First commit

--- src/public/addNews.php
+++ src/public/addNews.php
@@ -8,35 +8,10 @@
     <title>Document</title>
 </head>
 <body>
-<form method="post" enctype="multipart/form-data" action="newsList.php">
-    <label> <p> Название новости: </p> <input name="name" type="text" > </label>
-    <label> <p> Краткое описание: </p> <input name="description" type="text"></label>
-    <label> <p> Дата создания: </p> <input name="date" datatype="ДД-ММ-ГГГГ" value="21-01-2022" min="21-01-2022" max="22-02-2022"> </label>
-    <label> <p> Статус: </p> <input name="status" type="radio" value="Черновик" >
-            <input name="status" type="radio" value="Опубликовано">
-    </label>
-    <label> <p> Текст новости: </p> <input name="text" type="text"> </label>
-    <label> <p> Автор новости: </p> <input name="author" type="text"> </label>
-    <label> <p> Загрузка картинки: </p> <input name="image" type="file"> </label>
-</form>
 <?php
 $name=$description=$status=$text=$author=$image="";
 $nameError=$descriptionError=$statusError=$textError=$authorError=$imageError="";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = test_input($_POST["name"]);
-    $description = test_input($_POST["description"]);
-    $status = test_input($_POST["status"]);
-    $text = test_input($_POST["text"]);
-    $author = test_input($_POST["authors"]);
-}
-
-function test_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-}
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($_POST["name"])) {
         $nameError = "Введите имя!";
@@ -64,11 +39,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $author = test_input($_POST["authors"]);
     }
     if (empty($_POST["image"])) {
-        $imageError = "Добавте картинку";
+        $imageError = "Добавьте картинку";
     } else {
         $image = test_input($_POST["image"]);
     }
 }
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 ?>
+<form method="post" enctype="multipart/form-data" action="newsList.php">
+    <label> <p> Название новости: </p> <input name="name" type="text" > </label>
+        <div>* <?php echo $nameError;?></div>
+    <label> <p> Краткое описание: </p> <input name="description" type="text" minlength="20"></label>
+        <div>* <?php echo $descriptionError;?></div>
+    <label> <p> Дата создания: </p> <input name="date" datatype="ДД-ММ-ГГГГ" value="21-01-2022" min="21-01-2022" max="22-02-2022"> </label>
+    <label> <p> Статус: </p> <input name="status" type="radio" value="Черновик" >
+            <input name="status" type="radio" value="Опубликовано">
+    </label>
+        <div>* <?php echo $statusError;?></div>
+    <label> <p> Текст новости: </p> <input name="text" type="text" minlength="100"> </label>
+        <div>* <?php echo $textError;?></div>
+    <label> <p> Автор новости: </p> <input name="author" type="text"> </label>
+        <div>* <?php echo $authorError;?></div>
+    <label> <p> Загрузка картинки: </p> <input name="image" type="file"> </label>
+        <div>* <?php echo $imageError;?></div>
+    <label> <input type="submit" value="Опубликовать"> </label>
+</form>
+
 </body>
 </html>
